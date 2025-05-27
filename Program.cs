588c4f6c0af68cf3065527280d5ab1565a8a4f78
“thechers” <matiass.fm13@gmail.com>
2025-05-27 12:40:33 -0300

    Ejercicio 1 Realizado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:" +a);
+Console.WriteLine("valor de b:"+b);
+
+//Ejercicio 1
+Console.WriteLine("Ingrese un numero:");
+string entrada = Console.ReadLine();
+int num; 
+bool resultado = int.TryParse(entrada, out num);
+
+if (resultado && num > 0)
+{
+    Console.WriteLine("Es un numero");
+    int invertido = 0;
+    while (num > 0)
+    {
+        int digito = num % 10;
+        invertido = (invertido * 10) + digito;
+        num /= 10;
+    }
+    Console.WriteLine(invertido);
+
+}
+else
+{
+    Console.WriteLine("No es un numero");
+}
+
+
